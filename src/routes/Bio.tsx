7b7f64f8cd6d7ecdbc087c38456a934ad5cfc827
Alexander Eckinger <eckinger@Alexanders-MacBook-Air.local>
2025-05-03 18:29:00 -0500

    trying to add deployment workflow

--- src/routes/Bio.tsx
+++ src/routes/Bio.tsx
@@ -9,7 +9,7 @@ const Bio: React.FC = () => {
             <h1 className="text-4xl font-bold mb-5 mt-40 text-center">
               <span className="text-highlight">Bio</span>
             </h1>
-            <p className="text-1xl mb-5 text-left px-20 font-signika">
+            <p className="text-1xl mb-5 text-left max-w-prose font-signika">
               As of June 2025, I will be a graduate of the University 
               of Chicago. I will be working at an energy startup based
               out of Chicago, but principally living and working from the
