c56df2fb2a76f5cbba3e8b890809424291670411
Alexander Eckinger <eckinger@Alexanders-MacBook-Air.local>
2025-05-03 20:15:38 -0500

    skeleton of Photos.tsx. no photo rendering yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './routes/Welcome';
+import Photos from './routes/Photos';
 import Bio from './routes/Bio';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Router>
           <Routes>
               <Route path="/" element={<Welcome />} />
+              <Route path="/photos" element={<Photos />} />
               <Route path="/bio" element={<Bio />} />
               <Route path="*" element={<Welcome />} />
           </Routes>
