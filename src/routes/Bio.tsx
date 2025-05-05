9d72556090b74b99bf514a0977548dd79996910f
Alexander Eckinger <eckinger@Alexanders-MacBook-Air.local>
2025-05-05 17:47:39 -0500

    changed bio

--- src/routes/Bio.tsx
+++ src/routes/Bio.tsx
@@ -9,9 +9,9 @@ const Bio: React.FC = () => {
               <span className="text-highlight">Bio</span>
             </h1>
             <p className="text-1xl mb-5 text-left max-w-prose font-signika btn-shadow btn-main-no-hover">
-              In June 2025, I graduated from the University of Chicago with
-              a degree in computer science. After moving back to the SF Bay,
-              I began work for a Chicago-based energy startup.
+              In June 2025, I will graduate from the University of Chicago with
+              a degree in computer science. This summer I will work for a
+              Chicago-based energy startup.
             </p>
           </div>
         </BaseLayout>
