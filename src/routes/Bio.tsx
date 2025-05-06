c5e3a10bd4c7e65b1305db4db309eecd6e1054ca
Alexander Eckinger <eckinger@Alexanders-MacBook-Air.local>
2025-05-05 19:43:47 -0500

    made bio box behavior on mobile better

--- src/routes/Bio.tsx
+++ src/routes/Bio.tsx
@@ -8,14 +8,16 @@ const Bio: React.FC = () => {
             <h1 className="text-4xl font-bold mb-5 mt-40 text-center">
               <span className="text-highlight font-rubik">Bio</span>
             </h1>
-            <div className="relative inline-block">
-              <span className="btn-shadow"></span>
-              <span className="btn-main-no-hover">
-                <p className="text-1xl text-left max-w-prose font-rubik">
-                  In June 2025, I will graduate from the University of Chicago with
-                  a degree in computer science. This summer I will work for a
-                  Chicago-based energy startup.
-                </p></span>
+            <div className="px-4 w-full md:w-3/4 lg:w-2/3 max-w-3xl mx-auto">
+              <div className="relative inline-block w-full">
+                <span className="btn-shadow"></span>
+                <span className="btn-main-no-hover">
+                  <p className="text-1xl text-left max-w-prose font-rubik">
+                    In June 2025, I will graduate from the University of Chicago with
+                    a degree in computer science. This summer I will work for a
+                    Chicago-based energy startup.
+                  </p></span>
+              </div>
             </div>
           </div>
         </BaseLayout>
