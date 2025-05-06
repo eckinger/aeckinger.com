14cd273b647ecee4c76d22597beca11fb953bc2f
eckinger <62495359+eckinger@users.noreply.github.com>
2025-05-05 22:48:24 -0500

    Update Bio.tsx

--- src/routes/Bio.tsx
+++ src/routes/Bio.tsx
@@ -13,10 +13,18 @@ const Bio: React.FC = () => {
                 <span className="btn-shadow"></span>
                 <span className="btn-main-no-hover">
                   <p className="text-1xl text-left max-w-prose font-rubik font-medium">
-                    In June 2025, I will graduate from the University of Chicago with
-                    a degree in computer science. This summer I will work for a
-                    Chicago-based energy startup.
-                  </p></span>
+                    In June 2025, I graduate from the University of Chicago with a
+                    degree in computer science. I'm going to go back home to the SF
+                    bay and, at least for the summer, work for an energy startup.
+                  </p>
+                  <p className="text-1xl text-left max-w-prose font-rubik font-medium">
+                    In my free time, I love to rock climb, write cursive, read,
+                    hang out with my friends, and slackline. I've recently made
+                    my 2012 kid self proud and rekindled my love for the SF Giants,
+                    so I'd like to go to many games this summer (hopefully SJ Giants
+                    games too).
+                  </p>
+                </span>
               </div>
             </div>
           </div>
