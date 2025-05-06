c1fe62417dd3c7c73a4cfe0ea3a9e34c32295774
Alexander Eckinger <eckinger@Alexanders-MacBook-Air.local>
2025-05-05 19:06:22 -0500

    added shadow to bio box

--- src/routes/Bio.tsx
+++ src/routes/Bio.tsx
@@ -8,11 +8,15 @@ const Bio: React.FC = () => {
             <h1 className="text-4xl font-bold mb-5 mt-40 text-center">
               <span className="text-highlight">Bio</span>
             </h1>
-            <p className="text-1xl mb-5 text-left max-w-prose font-signika btn-shadow btn-main-no-hover">
-              In June 2025, I will graduate from the University of Chicago with
-              a degree in computer science. This summer I will work for a
-              Chicago-based energy startup.
-            </p>
+            <div className="relative inline-block">
+              <span className="btn-shadow"></span>
+              <span className="btn-main-no-hover">
+                <p className="text-1xl text-left max-w-prose font-signika">
+                  In June 2025, I will graduate from the University of Chicago with
+                  a degree in computer science. This summer I will work for a
+                  Chicago-based energy startup.
+                </p></span>
+            </div>
           </div>
         </BaseLayout>
     );
