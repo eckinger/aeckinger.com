8619ad3e62a40a6cbc2b648841139202f63a2bc4
Alexander Eckinger <eckinger@Alexanders-MacBook-Air.local>
2025-06-19 15:03:37 -0700

    REFACTOR: changed bio

--- src/routes/Bio.tsx
+++ src/routes/Bio.tsx
@@ -1,41 +1,42 @@
-import React from 'react';
-import BaseLayout from '../components/BaseLayout';
+import React from "react";
+import BaseLayout from "../components/BaseLayout";
 
 const Bio: React.FC = () => {
-    return (
-        <BaseLayout showHomeLink={true}>
-          <div className="min-h-screen flex flex-col items-center">
-            <h1 className="text-4xl font-bold mb-5 mt-40 text-center">
-              <span className="text-highlight font-rubik">Bio</span>
-            </h1>
-            <div className="px-4 w-full md:w-3/4 lg:w-2/3 max-w-3xl mx-auto">
-              <div className="relative inline-block w-full">
-                <span className="btn-shadow"></span>
-                <span className="btn-main-no-hover">
-                  <p className="text-1xl text-left max-w-prose font-rubik font-normal">
-                    In June 2025, I graduate from the University of Chicago with a
-                    degree in computer science. I'm returning home to the SF bay
-                    and, at least for the summer, working for an energy startup.
-                  </p>
-                  <br></br>
-                  <p className="text-1xl text-left max-w-prose font-rubik font-normal">
-                    In my free time, I love to rock climb, slackline, write in cursive,
-                    read, and hang out with my friends. I've recently made
-                    my 2012 kid self proud and rekindled my love for the SF Giants,
-                    so I'd like to go to many games this summer (hopefully SJ Giants
-                    games too).
-                  </p>
-                  <br></br>
-                  <p className="text-1xl text-left max-w-prose font-rubik font-normal">
-                    This website is hosted on my raspberry pi 🥰 and written by me.
-                    WIP.
-                  </p>
-                </span>
-              </div>
-            </div>
+  return (
+    <BaseLayout showHomeLink={true}>
+      <div className="min-h-screen flex flex-col items-center">
+        <h1 className="text-4xl font-bold mb-5 mt-40 text-center">
+          <span className="text-highlight font-rubik">Bio</span>
+        </h1>
+        <div className="px-4 w-full md:w-3/4 lg:w-2/3 max-w-3xl mx-auto">
+          <div className="relative inline-block w-full">
+            <span className="btn-shadow"></span>
+            <span className="btn-main-no-hover">
+              <p className="text-1xl text-left max-w-prose font-rubik font-normal">
+                In June 2025, I graduated from the University of Chicago with a
+                degree in computer science. I'm now home in the SF bay–this
+                summer, I am working for an energy startup. Not sure what comes
+                after lol
+              </p>
+              <br></br>
+              <p className="text-1xl text-left max-w-prose font-rubik font-normal">
+                In my free time, I love to rock climb, lift weights, slackline,
+                write letters and postcards, read, and hang out with my friends.
+                I've recently made my 2012 kid self proud and rekindled my love
+                for the SF Giants, so I'd like to go to many games this summer
+                (hopefully SJ Giants games too).
+              </p>
+              <br></br>
+              <p className="text-1xl text-left max-w-prose font-rubik font-normal">
+                This website is hosted on my raspberry pi 🥰 and written by me.
+                WIP.
+              </p>
+            </span>
           </div>
-        </BaseLayout>
-    );
+        </div>
+      </div>
+    </BaseLayout>
+  );
 };
 
 export default Bio;
