aec987c5f6759cd9bc4d49dace4b05a88fb359b9
eckinger <62495359+eckinger@users.noreply.github.com>
2025-07-28 13:09:03 -0500

    Update Welcome.tsx

--- src/routes/Welcome.tsx
+++ src/routes/Welcome.tsx
@@ -10,15 +10,9 @@ const Welcome: React.FC = () => {
           <span className="text-highlight font-rubik">Alexander Eckinger</span>
         </h1>
         <div className="flex flex-col items-center space-y-5">
-          {/*
-                      <Link to="/photos" className="relative">
-                        <span className="btn-shadow"></span>
-                        <span className="btn-main font-rubik">Photography</span>
-                      </Link>
-                    */}
           <a href="https://brackishwater.aeckinger.com" className="relative">
             <span className="btn-shadow"></span>
-            <span className="btn-main font-rubik">Photography</span>
+            <span className="btn-main font-rubik">Brackish Water</span>
           </a>
           <a href="https://www.goodreads.com/eckinger" className="relative">
             <span className="btn-shadow"></span>
