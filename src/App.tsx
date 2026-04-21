a44615ca8735393e407cb32535fea25ff21cbd81
Alexander Eckinger <eckinger@Alexanders-MacBook-Air.local>
2026-04-21 16:49:40 -0700

    grid but photos resize weird

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Welcome from './routes/Welcome';
-import Bio from './routes/Bio';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Welcome from "./routes/Welcome";
+import Photos from "./routes/Photos";
+import Bio from "./routes/Bio";
 
 function App() {
   return (
-      <Router>
-          <Routes>
-              <Route path="/" element={<Welcome />} />
-              <Route path="/bio" element={<Bio />} />
-              <Route path="*" element={<Welcome />} />
-          </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/photos" element={<Photos />} />
+        <Route path="/bio" element={<Bio />} />
+        <Route path="*" element={<Welcome />} />
+      </Routes>
+    </Router>
   );
 }
 
