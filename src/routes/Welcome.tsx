dedef1b8736f3e4e9ce18979b74ab080c670967a
Alexander Eckinger <eckinger@Alexanders-MacBook-Air.local>
2026-04-22 14:29:51 -0700

    changed photo button

--- src/routes/Welcome.tsx
+++ src/routes/Welcome.tsx
@@ -10,13 +10,9 @@ const Welcome: React.FC = () => {
           <span className="text-highlight font-rubik">Alexander Eckinger</span>
         </h1>
         <div className="flex flex-col items-center space-y-5">
-          <Link to="/photos" className="relative group">
+          <Link to="/photos" className="relative">
             <span className="btn-shadow"></span>
-            <span className="btn-main font-rubik">Brackish Water</span>
-            <div className="absolute left-full top-1/2 transform -translate-y-1/2 ml-3 bg-gray-800 text-white px-3 py-2 rounded-md text-sm whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-10">
-              Photos
-              <div className="absolute right-full top-1/2 transform -translate-y-1/2 w-0 h-0 border-t-4 border-b-4 border-r-4 border-t-transparent border-b-transparent border-r-gray-800"></div>
-            </div>
+            <span className="btn-main font-rubik">Photography</span>
           </Link>
           <a href="https://www.goodreads.com/eckinger" className="relative">
             <span className="btn-shadow"></span>
