8fe05916d45085703485f7a7d3a8a2b511f011e9
Alexander Eckinger <eckinger@Alexanders-MacBook-Air.local>
2026-04-23 11:19:00 -0700

    made mobile height for photos box taller

--- src/routes/Photos.tsx
+++ src/routes/Photos.tsx
@@ -22,7 +22,7 @@ const Photos: React.FC = () => {
             {" "}
             {/* Added w-full here */}
             <span className="btn-shadow"></span>
-            <div className="bg-white p-2 md:p-4 rounded-sm border-2 border-black relative h-[60vh] md:h-[80vh] overflow-y-scroll custom-scrollbar w-full">
+            <div className="bg-white p-2 md:p-4 rounded-sm border-2 border-black relative h-[70vh] md:h-[80vh] overflow-y-scroll custom-scrollbar w-full">
               <MasonryPhotoAlbum
                 photos={photos}
                 columns={(containerWidth) => {
